80e25aa799229ce4930aa2644f78a136198ae3e5
AlisiaSmith <anya0192837465@ya.ru>
2018-09-17 21:22:11 +0700

    add lib math.h

--- EVMlab1/Project1/Source.c
+++ EVMlab1/Project1/Source.c
@@ -1,18 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<math.h>
 
 int main(int argc, char **argv)
 {
 	unsigned int N = (unsigned int)atoi(argv[1]);
 	long double Pi = 0;
 	for (unsigned int i = 0; i <= N; i++)
-	{
-		int k;
-		if (i % 2 == 0) k = 1;
-		else k = -1;
-		Pi = Pi + 4.0 * ((float) k / (2.0 * (float)i + 1.0));
-	}
-	//Pi = Pi * (float)4;
+		Pi = Pi + 4.0 * ((float) pow(-1, i) / (2.0 * (float)i + 1.0));
 	printf("%Lf \n", Pi);
 	return 0;
 }
